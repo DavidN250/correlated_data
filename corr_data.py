e278c4393e8d009e17947d5e65b2f1a968c8551d
David NIYITEGEKA <38277519+DavidN250@users.noreply.github.com>
2023-04-19 16:28:13 +0200

    Update corr_data.py

--- corr_data.py
+++ corr_data.py
@@ -1,9 +1,9 @@
+#import libraries
+
 import numpy as np
 from numpy.random import multivariate_normal
-# import pandas as pd
-# import openpyxl as xls
-# import matplotlib.pyplot as plt
 
+#Declare the correlation matrix, it has to be a square matrix
 
 cov_matrix =[[1,0.21,0.56,0.36,0.67],
              [0.21,1,0.68,0.15,0.64],
@@ -11,15 +11,15 @@ cov_matrix =[[1,0.21,0.56,0.36,0.67],
              [0.36,0.15,0.11,1,0.41],
              [0.67,0.64,0.79,0.411,1]]
 
-data = multivariate_normal([0,0,0,0,0],cov_matrix,size=3000)
-np.savetxt("data.csv",data,delimiter=",")
+#Generate Data using multivariate_normal
 
-# for i in range(29):
-#    gen_arr = multivariate_normal([0,0,0,0,0],cov_matrix,size=100)
-#    data = np.concatenate((data,gen_arr),axis=0)
+data = multivariate_normal([0,0,0,0,0],cov_matrix,size=100) #this will generate 100 random values
 
-for d in data:
-    print(d)
+#Save data in csv file
 
-# print(data)
-# print(data.shape)
+np.savetxt("generated_data.csv",data,delimiter=",")
+
+#Display data in console if you want
+
+for record in data:
+    print(record)
